33cbb88b7ad62fc60d33d4078fc6babf720dbff6
Kyle Goodwin <kyle.goodwin93@gmail.com>
2021-11-09 22:42:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', 
+    e => cb(e)
+
+)
+
+function cb(event) { 
+    let text = document.getElementById('text')
+    text.textContent = 'this is fackin coo'
+    
+}
+
+
